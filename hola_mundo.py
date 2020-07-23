888538ac9520edcfd178f36768b80fef51f1f04c
Dian Delph <da.dnvl@live.com>
2020-07-22 21:19:49 -0500

    Agrega decoradores para evitar múltiples pestañas

--- hola_mundo.py
+++ hola_mundo.py
@@ -6,20 +6,25 @@ class HelloWorld(unittest.TestCase):
 
 	#va a ejecutar todo lo necesario antes de hacer una prueba
 	#va a preparar el entorno de la prueba
-	def setUp(self):
-		self.driver = webdriver.Chrome(executable_path = r'.\chromedriver.exe')
-		driver = self.driver
+	@classmethod  # para que abra las pestañas en una misma ventana
+	def setUpClass(cls):
+		cls.driver = webdriver.Chrome(executable_path = r'.\chromedriver.exe')
+		driver = cls.driver
 		driver.implicitly_wait(10)
 	
 	#aquí va lo que queremos que la prueba haga
 	def test_hello_world(self):
 		driver = self.driver
 		driver.get('https://platzi.com/')
-	
+
+	def test_visit_wikipedia(self):
+		self.driver.get('https://es.wikipedia.org/')
+
 	#cierra el navegador para evitar 
 	#fuga de recursos
-	def tearDown(self):
-		self.driver.quit()
+	@classmethod #decorador para que no se cierre la ventana después de la primera prueba
+	def tearDownClass(cls):
+		cls.driver.quit()
 
 
 
