ff2d9b08b1f8fcda3f94df252b2b9620e62aef7b
Dian Delph <da.dnvl@live.com>
2020-07-22 20:57:37 -0500

    Cambia nombre Agrega prueba

---
+++ hola_mundo.py
@@ -0,0 +1,29 @@
+import unittest
+from pyunitreport import HTMLTestRunner
+from selenium import webdriver
+
+class HelloWorld(unittest.TestCase):
+
+	#va a ejecutar todo lo necesario antes de hacer una prueba
+	#va a preparar el entorno de la prueba
+	def setUp(self):
+		self.driver = webdriver.Chrome(executable_path = r'D:\chromedriver.exe')
+		driver = self.driver
+		driver.implicitly_wait(10)
+	
+	#aquí va lo que queremos que la prueba haga
+	def test_hello_world(self):
+		driver = self.driver
+		driver.get('https://platzi.com/')
+	
+	#cierra el navegador para evitar 
+	#fuga de recursos
+	def tearDown(self):
+		self.driver.quit()
+
+
+#
+if __name__ == "__main__":
+	unittest.main(verbosity = 2, testRunner = HTMLTestRunner(output = 'reportes', report_name='hello-world-report'))
+
+
